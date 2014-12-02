ee4460dcdce96e3e3a6471832d435f71b17f2844
Moerafaat <moerafaat@aucegypt.edu>
2014-12-02 21:44:09 +0200

    Changes to GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,24 +10,29 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     ui->setupUi(this);
     QIcon icon(":/images/logo.png");
     QWidget::setWindowIcon(icon);
+
     ui->textedit_editor->setReadOnly(true);
     ui->plaintextedit_log->setReadOnly(true);
+
+    ui->tablewidget_regfile->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->tablewidget_regfile->setColumnCount(1);
     ui->tablewidget_regfile->setRowCount(32);
     ui->tablewidget_regfile->setHorizontalHeaderItem(0, new QTableWidgetItem("Content"));
     for(int i=0; i<ui->tablewidget_regfile->rowCount(); i++){
         ui->tablewidget_regfile->setVerticalHeaderItem(i, new QTableWidgetItem(QString::number(i)));
     }
+
+    ui->tablewidget_memory->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->tablewidget_memory->setColumnCount(1);
     ui->tablewidget_memory->setRowCount(32);
     ui->tablewidget_memory->setHorizontalHeaderItem(0, new QTableWidgetItem("Content"));
     for(int i=0; i<ui->tablewidget_memory->rowCount(); i++){
         ui->tablewidget_memory->setVerticalHeaderItem(i, new QTableWidgetItem(QString::number(i)));
     }
+
     ui->tablewidget_pipeline->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    ui->tablewidget_pipeline->setColumnCount(1);
-    ui->tablewidget_pipeline->setHorizontalHeaderItem(0, new QTableWidgetItem("Instruction"));
-    //ui->tablewidget_pipeline->setShowGrid(false);
+    ui->tablewidget_pipeline->setColumnCount(0);
+    ui->tablewidget_pipeline->setShowGrid(false);
 }
 
 MainWindow::~MainWindow(){
@@ -64,8 +69,10 @@ void MainWindow::on_actionStep_triggered(){
     map.insert("EX", 2); color.insert("EX", QColor::fromRgb(255,0,0));
     map.insert("MEM", 3); color.insert("MEM", QColor::fromRgb(0,204,0));
     map.insert("WB", 4); color.insert("WB", QColor::fromRgb(255,255,0));
+
     QTableWidgetItem *item;
     QFont font("Helvetica", 10, QFont::Bold);
+
     for(int i=0; i<ui->tablewidget_regfile->rowCount(); i++){
         item = new QTableWidgetItem(QString::number(rand()%100));
         item->setTextAlignment(Qt::AlignCenter);
@@ -76,34 +83,42 @@ void MainWindow::on_actionStep_triggered(){
         item->setTextAlignment(Qt::AlignCenter);
         ui->tablewidget_memory->setItem(i, 0, item);
     }
+
     bool is_stall;
     int pc, stall_count, column_count;
-    static int cycle = 0;
+    static int cycle = -1;
     is_stall = false;
-    stall_count = -1;
+    stall_count = 0;
     column_count = 5;
     pc = 0;
+    if (cycle == 7){
+        is_stall = true;
+        stall_count = 2;
+    }
     QVector<QString> inst;
     inst.push_back("XOR 1,2,3");
     ui->tablewidget_pipeline->insertColumn(ui->tablewidget_pipeline->columnCount());
-    ui->tablewidget_pipeline->setHorizontalHeaderItem(ui->tablewidget_pipeline->columnCount()-1, new QTableWidgetItem("Cycle " + QString::number(++cycle)));
+    ui->tablewidget_pipeline->setHorizontalHeaderItem(ui->tablewidget_pipeline->columnCount()-1, new QTableWidgetItem("Cycle " + QString::number((++cycle + 1))));
+
     if(!is_stall){
         ui->tablewidget_pipeline->insertRow(ui->tablewidget_pipeline->rowCount());
         item = new QTableWidgetItem(inst[pc]);
-        ui->tablewidget_pipeline->setItem(ui->tablewidget_pipeline->rowCount()-1, 0, item);
+        ui->tablewidget_pipeline->setVerticalHeaderItem(ui->tablewidget_pipeline->rowCount()-1, item);
         item = new QTableWidgetItem(map.key(0));
         item->setTextAlignment(Qt::AlignCenter);
         item->setFont(font);
         ui->tablewidget_pipeline->setItem(ui->tablewidget_pipeline->rowCount()-1, cycle, item);
     }
+
     for(int i=0; i<column_count; i++){
-        if(ui->tablewidget_pipeline->rowCount()-1-i-1 < 0) break;
-        item = new QTableWidgetItem(map.key(map[ui->tablewidget_pipeline->item(ui->tablewidget_pipeline->rowCount()-1-i-1, cycle-1)->text()]+1));
+        if(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall) < 0 || ui->tablewidget_pipeline->item(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall), cycle-1)->text() == "") break;
+        item = new QTableWidgetItem(map.key(map[ui->tablewidget_pipeline->item(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall), cycle-1)->text()]+(i >= stall_count)));
         item->setTextAlignment(Qt::AlignCenter);
         item->setFont(font);
-        item->setForeground(color[map.key(map[ui->tablewidget_pipeline->item(ui->tablewidget_pipeline->rowCount()-1-i-1, cycle-1)->text()]+1)]);
-        ui->tablewidget_pipeline->setItem(ui->tablewidget_pipeline->rowCount()-1-i-1, cycle, item);
+        item->setForeground(color[map.key(map[ui->tablewidget_pipeline->item(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall), cycle-1)->text()]+(i >= stall_count))]);
+        ui->tablewidget_pipeline->setItem(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall), cycle, item);
     }
-    QString message = "-Step. Cycle: " + QString::number(cycle) + " Rows: " + QString::number(ui->tablewidget_pipeline->rowCount()) + + " Columns: " + QString::number(ui->tablewidget_pipeline->columnCount());
+
+    QString message = "-Step. Cycle: " + QString::number(cycle+1) + " Rows: " + QString::number(ui->tablewidget_pipeline->rowCount()) + + " Columns: " + QString::number(ui->tablewidget_pipeline->columnCount());
     ui->plaintextedit_log->appendPlainText(message);
 }
