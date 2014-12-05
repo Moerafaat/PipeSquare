f3703400032baf62a06c8aa9d54aee69a68f7d11
Moerafaat <moerafaat@aucegypt.edu>
2014-12-05 12:53:59 +0200

    GUI changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,39 +4,80 @@
 #include <QMessageBox>
 #include <QTextStream>
 #include <QMap>
+#include <cmath>
+#include <QDebug>
 
-
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow){
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
     QIcon icon(":/images/logo.png");
     QWidget::setWindowIcon(icon);
 
     ui->textedit_editor->setReadOnly(true);
-    ui->plaintextedit_log->setReadOnly(true);
+    ui->textedit_log->setReadOnly(true);
 
     ui->tablewidget_regfile->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->tablewidget_regfile->setColumnCount(1);
     ui->tablewidget_regfile->setRowCount(32);
     ui->tablewidget_regfile->setHorizontalHeaderItem(0, new QTableWidgetItem("Content"));
-    for(int i=0; i<ui->tablewidget_regfile->rowCount(); i++){
+    for(int i=0; i<ui->tablewidget_regfile->rowCount(); i++)
         ui->tablewidget_regfile->setVerticalHeaderItem(i, new QTableWidgetItem(QString::number(i)));
-    }
 
     ui->tablewidget_memory->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->tablewidget_memory->setColumnCount(1);
     ui->tablewidget_memory->setRowCount(32);
     ui->tablewidget_memory->setHorizontalHeaderItem(0, new QTableWidgetItem("Content"));
-    for(int i=0; i<ui->tablewidget_memory->rowCount(); i++){
+    for(int i=0; i<ui->tablewidget_memory->rowCount(); i++)
         ui->tablewidget_memory->setVerticalHeaderItem(i, new QTableWidgetItem(QString::number(i)));
-    }
 
     ui->tablewidget_pipeline->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->tablewidget_pipeline->setColumnCount(0);
     ui->tablewidget_pipeline->setShowGrid(false);
+
+    QVector<QString> IFID_labels = {"Mnemonic", "Rs", "Rt", "Td", "Imm", "Jaddr"};
+    InitializeBuffer(ui->tablewidget_IFID,IFID_labels);
+
+    QVector<QString> IDEX_labels = {"WE", "WAddr", "Data", "MemRW", "ALUMem", "ALUOp", "Op1", "Op2"};
+    InitializeBuffer(ui->tablewidget_IDEX,IDEX_labels);
+
+    QVector<QString> EXMEM_labels = {"WE", "WAddr", "WData", "MemRW", "ALUMem", "MemAddr"};
+    InitializeBuffer(ui->tablewidget_EXMEM,EXMEM_labels);
+
+    QVector<QString> MEMWB_labels = {"WE", "WAddr", "WData"};
+    InitializeBuffer(ui->tablewidget_MEMWB,MEMWB_labels);
 }
 
 MainWindow::~MainWindow(){
     delete ui;
+}
+
+void MainWindow::InitializeBuffer(QTableWidget* buffer, QVector<QString> labels){
+    QTableWidgetItem *item;
+    QFont font("Helvetica", 9, QFont::Bold);
+    buffer->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    buffer->verticalHeader()->hide();
+    buffer->setRowCount(4);
+    buffer->setColumnCount(4);
+    for (int i=0; i<labels.size(); i++){
+        item = new QTableWidgetItem(labels[i]);
+        item->setForeground(QColor::fromRgb(255,0,0));
+        item->setFont(font);
+        item->setTextAlignment(Qt::AlignCenter);
+        if(i<4) buffer->setItem(0, i, item);
+        else buffer->setItem(2, i-4, item);
+    }
+    for(int i=0; i<4; i++) buffer->setColumnWidth(i, 69);
+}
+
+void MainWindow::SetBuffer(QTableWidget* buffer, QVector<QString> values)
+{
+    QTableWidgetItem *item;
+    for (int i=0; i<values.size(); i++){
+        item = new QTableWidgetItem(values[i]);
+        item->setTextAlignment(Qt::AlignCenter);
+        if(i<4) buffer->setItem(1, i, item);
+        else buffer->setItem(3, i-4, item);
+    }
 }
 
 void MainWindow::on_actionOpen_triggered(){
@@ -45,31 +86,42 @@ void MainWindow::on_actionOpen_triggered(){
     if(file.open(QFile::ReadOnly)){
         QTextStream stream(&file);
         QString content, message;
-        while(!stream.atEnd()){
-            content.append(stream.readLine() + '\n');
-        }
-        message = "-File at path: " + path + " has been successfully opened.";
+        while(!stream.atEnd()) content.append(stream.readLine() + '\n');
         ui->textedit_editor->setText(content);
-        ui->plaintextedit_log->appendPlainText(message);
         file.close();
+
+        message = "-File at path: " + path + " has been successfully opened.";
+        ui->textedit_log->setTextColor(QColor::fromRgb(0,102,0));
+        ui->textedit_log->append(message);
     }
 }
 
 void MainWindow::on_actionCompile_triggered(){
-    instructions = parser.parse(file);
+    try{
+        instructions = parser.parse(file);
+    }catch(...){
+        QString message = "-File has failed to compile";
+        ui->textedit_log->setTextColor(QColor::fromRgb(204,0,0));
+        ui->textedit_log->append(message);
+        return;
+    };
+
     QString message = "-File has been successfully compiled";
-    ui->plaintextedit_log->appendPlainText(message);
+    ui->textedit_log->setTextColor(QColor::fromRgb(0,102,0));
+    ui->textedit_log->append(message);
 }
 
 void MainWindow::on_actionStep_triggered(){
     QMap<QString, int> map;
     QMap<QString, QColor> color;
+    bool is_stall;
+    int pc, stall_count, column_count;
+    static int cycle = -1;
     map.insert("IF", 0); color.insert("IF",QColor::fromRgb(0,0,0));
     map.insert("ID", 1); color.insert("ID", QColor::fromRgb(0,0,204));
     map.insert("EX", 2); color.insert("EX", QColor::fromRgb(255,0,0));
     map.insert("MEM", 3); color.insert("MEM", QColor::fromRgb(0,204,0));
     map.insert("WB", 4); color.insert("WB", QColor::fromRgb(255,255,0));
-
     QTableWidgetItem *item;
     QFont font("Helvetica", 10, QFont::Bold);
 
@@ -78,15 +130,26 @@ void MainWindow::on_actionStep_triggered(){
         item->setTextAlignment(Qt::AlignCenter);
         ui->tablewidget_regfile->setItem(i, 0, item);
     }
+
     for(int i=0; i<ui->tablewidget_memory->rowCount(); i++){
         item = new QTableWidgetItem(QString::number(rand()%100));
         item->setTextAlignment(Qt::AlignCenter);
         ui->tablewidget_memory->setItem(i, 0, item);
     }
+    QVector<QString> IFID_values = {"ADD", "2", "3", "5", "25", "1"};
+    if(cycle%2) IFID_values = {"ADD", "2", "3", "5", "25", "1"};
+    else IFID_values = {"SUB", "4", "12", "9", "5", "124"};
+    SetBuffer(ui->tablewidget_IFID,IFID_values);
 
-    bool is_stall;
-    int pc, stall_count, column_count;
-    static int cycle = -1;
+    QVector<QString> IDEX_values = {"1", "9", "21", "1", "0", "2", "10", "15"};
+    SetBuffer(ui->tablewidget_IDEX,IDEX_values);
+
+    QVector<QString> EXMEM_values = {"1", "9", "21", "1", "0", "5"};
+    SetBuffer(ui->tablewidget_EXMEM,EXMEM_values );
+
+    QVector<QString> MEMWB_values = {"1", "9", "21"};
+    SetBuffer(ui->tablewidget_MEMWB,MEMWB_values);
+
     is_stall = false;
     stall_count = 0;
     column_count = 5;
@@ -98,7 +161,7 @@ void MainWindow::on_actionStep_triggered(){
     QVector<QString> inst;
     inst.push_back("XOR 1,2,3");
     ui->tablewidget_pipeline->insertColumn(ui->tablewidget_pipeline->columnCount());
-    ui->tablewidget_pipeline->setHorizontalHeaderItem(ui->tablewidget_pipeline->columnCount()-1, new QTableWidgetItem("Cycle " + QString::number((++cycle + 1))));
+    ui->tablewidget_pipeline->setHorizontalHeaderItem(ui->tablewidget_pipeline->columnCount()-1, new QTableWidgetItem("Cyc " + QString::number((++cycle + 1))));
 
     if(!is_stall){
         ui->tablewidget_pipeline->insertRow(ui->tablewidget_pipeline->rowCount());
@@ -116,10 +179,13 @@ void MainWindow::on_actionStep_triggered(){
         item->setTextAlignment(Qt::AlignCenter);
         item->setFont(font);
         item->setForeground(color[map.key(map[ui->tablewidget_pipeline->item(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall), cycle-1)->text()]+(i >= stall_count))]);
-        if (i < stall_count && is_stall) item->setBackgroundColor(QColor::fromRgb(160,160,160));
+        if (i < stall_count && is_stall) item->setBackgroundColor(QColor::fromRgb(224,224,224));
         ui->tablewidget_pipeline->setItem(ui->tablewidget_pipeline->rowCount()-1-i-(!is_stall), cycle, item);
     }
 
+    ui->tablewidget_pipeline->resizeColumnsToContents();
+
     QString message = "-Step. Cycle: " + QString::number(cycle+1) + " Rows: " + QString::number(ui->tablewidget_pipeline->rowCount()) + + " Columns: " + QString::number(ui->tablewidget_pipeline->columnCount());
-    ui->plaintextedit_log->appendPlainText(message);
+    ui->textedit_log->setTextColor(QColor::fromRgb(204,0,0));
+    ui->textedit_log->append(message);
 }
